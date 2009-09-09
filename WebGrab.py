c34e4e3802bf41225b771eca2503f2fe9f3cae0e
Manuel Lagang <laganojunior@yahoo.com>
2009-09-08 22:12:03 -0700

    Migrated to Python 2.6
    Moved code from Python 3.1 to Python 2.6 in order to use NumPy,
    which currently does not support Python 3.

--- WebGrab.py
+++ WebGrab.py
@@ -1,18 +1,18 @@
-import urllib.request
+import urllib2
 import json
 import re
 
 def getAnimeList(username):
     apiUrl = "http://mal-api.com/animelist/" + username
-    f = urllib.request.urlopen(apiUrl)
-    j = json.loads(f.read().decode())
+    f = urllib2.urlopen(apiUrl)
+    j = json.loads(f.read())
     return j["anime"]
     
 def getUserId(username):
     profileUrl = "http://myanimelist.net/profile/" + username
-    f = urllib.request.urlopen(profileUrl)
+    f = urllib2.urlopen(profileUrl)
     
-    profileHtml = f.read().decode()
+    profileHtml = f.read()
     
     userIdRegEx = r'<a href="http://myanimelist.net/myfriends.php\?go=add&id=([0-9]+)" title=".*">Send Friend Request</a>'
     
@@ -26,11 +26,11 @@ def getUserId(username):
 def getRandomUsername():
     # Use the url for getting a random list
     url = "http://myanimelist.net/users.php?lucky=1"
-    f = urllib.request.urlopen(url)
+    f = urllib2.urlopen(url)
 
     # Extract the username from the title
     usernameRegex = r"<title>([\S]+)'s Anime List - MyAnimeList.net</title>"
-    match = re.search(usernameRegex, f.read().decode())
+    match = re.search(usernameRegex, f.read())
 
     if match is None:
         raise Exception("No regular expression match")
@@ -39,12 +39,12 @@ def getRandomUsername():
     
 def getRecentOnlineUsernames():
     url = "http://myanimelist.net/users.php"
-    f = urllib.request.urlopen(url)
+    f = urllib2.urlopen(url)
     
     # Extract all matches of a profile url
     usernameRegex = r'<a href="http://myanimelist.net/profile/([\S]+)">[\S]+</a>'
     
-    match = re.findall(usernameRegex, f.read().decode())
+    match = re.findall(usernameRegex, f.read())
     
     if match is None:
         return []
