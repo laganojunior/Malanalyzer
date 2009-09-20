451afa9be6555181948dbb6efc5488476d0c70b5
Manuel Lagang <jrthemaster@jrthemaster-laptop.(none)>
2009-09-19 17:27:52 -0700

    Added a in memory reader for databases
    The MalDB.MemReader class keeps a copy of a snapshot of the
    database in memory and allows quick access to all read functions
    that the MalDB class has. This avoids issues with accessing the
    hard drive too often when external changes are not necessary.

--- MalDB.py
+++ MalDB.py
@@ -1,6 +1,13 @@
 import sqlite3
+from Bimap import Bimap
 
 class MalDB:
+    """
+    This is the interface to the underlying sqlite3 database stored on the hard
+    disk. All I/O functions will apply directly to the database connection, so
+    changes outside of this connection are immediately visible. For changes made
+    in this connection to be viewable outside, call saveChanges()
+    """
     
     def __init__(self, dbName):
         self.conn = sqlite3.connect(dbName)
@@ -126,6 +133,100 @@ class MalDB:
             ratingMap[(row[0],row[1])] = row[2]
             
         return ratingMap
+
+
+class MemReader:
+    """
+    A memory reader for the sqlite3 database. It reads data from the database 
+    and stores a copy in memory, which can then be quickly accessed without 
+    accessing the hard drive. This does mean that changes made afterward to the
+    database are not viewable from the memory reader, so this should only be 
+    used when several accesses on a snapshot of the database is required. 
+    The reader supports all non-write operations of MalDB.
+    """
+
+    def __init__(self, dbName):
+        conn = sqlite3.connect(dbName)
+                
+        # Read the anime id bimap
+        c = conn.cursor()
+        c.execute("select id, name from animeIds")
+
+        animeIdPairs = [(row[0], row[1]) for row in c]
+        self.animeIdMap = Bimap(animeIdPairs, enforceBijection = False)
+        self.animeIds = [pair[0] for pair in animeIdPairs]
+        c.close()
+
+        # Read the user id bimap
+        c = conn.cursor()
+        c.execute("select id, name from userIds")
+
+        userIdPairs = [(row[0], row[1]) for row in c]
+        self.userIdMap = Bimap(userIdPairs, enforceBijection = False)
+        self.userIds = [pair[0] for pair in userIdPairs]
+        c.close()
+
+        # Read all ratings triples
+        c = conn.cursor()
+        c.execute("select userId, animeId, rating from ratings where rating <> 0")
+
+        ratingTriples = [(row[0], row[1], row[2]) for row in c]
+        c.close()
+
+        # Convert the ratings triples to the desired mappings
+        self.ratingsMatrix = {}
+        self.userRatingMap = {}
+        self.animeRatingMap = {}
+        for (userId, animeId, rating) in ratingTriples:
+            self.ratingsMatrix[(userId, animeId)] = rating
+
+            if userId not in self.userRatingMap:
+                self.userRatingMap[userId] = {}
+
+            self.userRatingMap[userId][animeId] = rating
+
+            if animeId not in self.animeRatingMap:
+                self.animeRatingMap[animeId] = {}
+            
+            self.animeRatingMap[animeId][userId] = rating
+
+    def getUserId(self, username):
+        return self.userIdMap.key(username)
+        
+    def getAnimeId(self, animename):
+        return self.animeIdMap.key(animename)
+        
+    def getAllUserIds(self):
+        return self.userIds
+        
+    def getAllAnimeIds(self):
+        return self.animeIds
+
+    def getUserName(self, userId):
+        return self.userIdMap.value(userId)
+        
+    def getAnimeName(self, animeId):
+        return self.animeIdMap.value(animeId)
+
+    def getAnimeRatingsForUser(self, userId):
+        if userId not in self.userRatingMap:
+            return {}
+
+        return self.userRatingMap[userId]
+        
+    def getUserRatingsForAnime(self, animeId):
+        if animeId not in self.animeRatingMap:
+            return {}
+
+        return self.animeRatingMap[animeId]
+        
+    def getRatingsMatrix(self):
+        return self.ratingsMatrix
+
+
+    
+
+    
             
         
             
