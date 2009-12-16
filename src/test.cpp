9dc894ad395eecb8f737a515ad3e51aac28fb849
Manuel Lagang <jrthemaster@jrthemaster-laptop.(none)>
2009-12-16 12:28:53 -0800

    Printed out top predicted ratings for recommendations

--- src/test.cpp
+++ src/test.cpp
@@ -3,45 +3,63 @@
 #include "AveragePredictionModel.h"
 #include "LinearModel.h"
 #include "Matrix.h"
+#include <cstdlib>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
 
 int main() 
 {
+    srand(time(0));
+ 
     MalDBReader db(string("Mal.db"));
 
-	string username = "laganojunior";
+    int userId = db.getUserId("lelouch9178");
 	
-	cout << "Username is: " << db.getUserName(db.getUserId(username)) << endl;
-
-	string animename = "Cowboy Bebop";
-	
-	cout << "Name is: " << db.getAnimeName(db.getAnimeId(animename)) << endl;
-
-	
-	unsigned int userId = db.getUserId(username);
-
-	const vector<unsigned int>& rated = db.getAnimeRated(userId);
-	const vector<double>& ratings = db.getAnimeRatings(userId);
-
-	assert(rated.size() == ratings.size());
-
-	for (int i = 0; i < rated.size(); i++)
-	{
-		cout << db.getAnimeName(rated[i]) << " " << ratings[i] << endl;
-	}
-
     AveragePredictionModel model;
 
     Matrix fullMatrix = db.getMatrix();
-    
-    Matrix trainingMatrix;
-    Matrix testMatrix;
 
-    fullMatrix.randomSplit(.95, trainingMatrix, testMatrix);
- 
-    model.train(trainingMatrix);
-    
-    cout << "RMSE is " << model.RMSE(testMatrix) << endl;
+    int numTries = 20;
+    double trainingRatio = .5;
 
-	cout << "hello" << endl;
+    double rmseSum = 0;
+   
+    for (int i = 0; i < numTries; i++)
+    {
+        Matrix trainingMatrix;
+        Matrix testMatrix;
+
+        fullMatrix.randomSplit(trainingRatio, trainingMatrix, testMatrix);
+     
+        model.train(trainingMatrix);
+
+        rmseSum += model.RMSE(testMatrix);
+    } 
+
+    cout << "RMSE is " << rmseSum / numTries << endl;
+
+
+    int numBest = 25;
+
+    vector<pair<double, int> > animePredicts;
+    animePredicts.resize(db.getNumAnime());
+
+    for (int i = 0; i < db.getNumAnime(); i++)
+    {        
+        animePredicts[i] = pair<double, int> (model.predict(userId, i), i);
+    }
+
+    sort(animePredicts.begin(), animePredicts.end());
+
+    for (int i = 0; i < numBest; i++)
+    {
+        int j = db.getNumAnime() - i - 1;
+
+        cout << db.getAnimeName(animePredicts[j].second) 
+             << " " <<  animePredicts[j].first << endl;
+    }
+
     return 0;
 }
