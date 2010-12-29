34f2e88ba752771a06d582769a61990f45571e0e
Manuel Lagang <laganojunior@gmail.com>
2010-12-28 22:50:14 -0800

    Removed unneeded indices

--- webapp/Entities.py
+++ webapp/Entities.py
@@ -2,16 +2,16 @@ from google.appengine.ext import db
 
 class User(db.Model):
     userid = db.IntegerProperty()
-    name = db.StringProperty()
+    name = db.StringProperty(indexed=False)
 
 class Anime(db.Model):
     animeid = db.IntegerProperty()
-    name = db.StringProperty()
+    name = db.StringProperty(indexed=False)
 
 class Rating(db.Model):
     user = db.ReferenceProperty(User)
     anime = db.ReferenceProperty(Anime)
-    rating = db.IntegerProperty()
+    rating = db.IntegerProperty(indexed=False)
 
 class QueueUser(db.Model):
     username = db.StringProperty()
