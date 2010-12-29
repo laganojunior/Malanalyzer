5008cd2e8667d95ddc198971d5649e0f652383e6
Manuel Lagang <laganojunior@gmail.com>
2010-12-28 21:30:51 -0800

    Added optional profiling output

--- webapp/main.py
+++ webapp/main.py
@@ -5,10 +5,36 @@ from Extractor import Extractor
 from Index import Index
 from FillQueue import FillQueue
 
-if __name__ == "__main__":
+# Profile mode. Can be "Print", "Log", or "None"
+PROFILE = "None"
+
+def main():
     application = webapp.WSGIApplication(
                                      [('/extract', Extractor),
                                       ('/fillqueue', FillQueue),
                                       ('/', Index)],
                                      debug=True)
     run_wsgi_app(application)
+
+if __name__ == "__main__":
+    if PROFILE == "Print":
+        import cProfile, pstats
+        prof = cProfile.Profile()
+        prof = prof.runctx("main()", globals(), locals())
+        print "<pre>"
+        stats = pstats.Stats(prof)
+        stats.sort_stats("cumulative")  # Or cumulative
+        stats.print_stats()  # 80 = how many to print
+        print "</pre>" 
+    elif PROFILE == "Log":
+        import cProfile, pstats, StringIO, logging
+        prof = cProfile.Profile()
+        prof = prof.runctx("main()", globals(), locals())
+        stream = StringIO.StringIO()
+        stats = pstats.Stats(prof, stream=stream)
+        stats.sort_stats("cumulative")  # Or cumulative
+        stats.print_stats()  # 80 = how many to print
+        # The rest is optional.
+        logging.info("Profile data:\n%s", stream.getvalue())
+    else:
+        main()
